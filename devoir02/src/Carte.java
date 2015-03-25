f678cd924343d49730c7300927a60955f2714272
Alexandre Lara <alexandrejml@live.com>
2015-03-25 01:02:33 -0400

    Entire class was concluded.

--- devoir02/src/Carte.java
+++ devoir02/src/Carte.java
@@ -1,3 +1,8 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.Random;
+
 import javax.swing.JComponent;
 
 public abstract class Carte extends JComponent {
@@ -26,7 +31,7 @@ public abstract class Carte extends JComponent {
 	 * 
 	 * @return
 	 */
-	boolean estMontree(){
+	public boolean estMontree(){
 		if(isFace)
 			return true;
 		return false;
@@ -36,12 +41,57 @@ public abstract class Carte extends JComponent {
 	 * 
 	 * @return
 	 */
-	boolean estCachee(){
+	public boolean estCachee(){
 		if(!isFace)
 			return true;
 		return false;
 	}
 	
+	public void montre(){
+		if(!isFace) isFace = true;
+		repaint();
+	}
+	
+	public void cache(){
+		if(isFace) isFace = false;
+		repaint();
+	}
+	
+	public void retourne(){
+		if(isFace) cache();
+		else montre();
+		repaint();
+	}
+	
+	public void paintVerso(Graphics2D g){
+		super.paintComponent(g);
+		g.setPaint(Color.BLACK);
+		g.fillRect(0, 0, getWidth(), getHeight());	
+	}
+	
+	public abstract void paintRecto(Graphics2D g);
+	
+	public abstract boolean rectoIdentique(Carte c);
+	
+	public abstract Object duplique();
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		if(isFace) paintRecto(g2);
+		else paintVerso(g2);
+	}
+	
+	public static void melangeCartes(Carte[] cartes){
+		Random random = new Random();
+	    for (int i = cartes.length - 1; i > 0; i--)
+	    {
+	      int cartesIndex = random.nextInt(i + 1);
+	      Carte carte = cartes[cartesIndex];
+	      cartes[cartesIndex] = cartes[i];
+	      cartes[i] = carte;
+	    }
+	}
 	
 	
 	
