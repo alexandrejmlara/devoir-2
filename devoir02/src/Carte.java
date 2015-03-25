108e82728addc987b2fec5a1e0f82bb183f595d2
Rodolpho Pinheiro <rodolphopinheiro@live.com>
2015-03-25 00:05:26 -0400

    changements classe Carte

--- devoir02/src/Carte.java
+++ devoir02/src/Carte.java
@@ -5,12 +5,22 @@ public abstract class Carte extends JComponent {
 	private boolean imageFaceRecto;
 	
 	/**
-	 * 
+	 * Un constructeur protected prenant un paramètre booléen  indiquant si la carte est sur sa face recto 
+	 * (côté image) ou verso (sur le dos, image cachée).
 	 * @param imageFaceRecto
 	 */
 	protected Carte (boolean imageFaceRecto){
 		this.imageFaceRecto=imageFaceRecto;
 	}
+	
+	/**
+	 * Un constructeur de copie protected prenant en paramètre une autre instance de Carte
+	 * @param c
+	 */
+	protected Carte (Carte c){
+		this.imageFaceRecto=c.imageFaceRecto;
+	}
+	
 	
 	/**
 	 * 
