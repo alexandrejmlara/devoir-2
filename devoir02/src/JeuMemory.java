9d68f1e4cffc2e56882fececa9f0002d729d270c
gabrielhv <gabriel.houlev@hotmail.com>
2015-03-28 01:38:52 -0400

    Update JeuMemory.java

--- devoir02/src/JeuMemory.java
+++ devoir02/src/JeuMemory.java
@@ -1,27 +1,18 @@
 
 public class JeuMemory {
-	 /**
-	 * coup : compteur pour le nombre de coups effectuÈ par le joueur
-	 */
-    private int coup = 0;
-    public void setCoup(int c){coup += c;} // Permet d'incr√©menter du nombre voulu
-    public int getCoup(){return coup;} // Retourne le nombre de coups
-    
-    
-    
     /** 
      * 
-     * @param nR : nombre de rangÈes de cartes
+     * @param nR : nombre de rang√©es de cartes
      * @param nC : nombre de colonnes de cartes
      * @param delaiAI : temps d'affichage initial des cartes
-     * @param delaiMP : temps de dÈlai lors d'une mauvaise paire
-     * @param theme : numÈro du th√®me choisi pour la partie
+     * @param delaiMP : temps de d√©lai lors d'une mauvaise paire
+     * @param theme : num√©ro du th√É¬®me choisi pour la partie
      */
     public void partie(int nR, int nC, int delaiAI, int delaiMP, int theme)
     {
         
     	
-    	// Choix du theme
+    	// Choix du theme de carte
     	/*
     	Carte carte;
     	switch (theme) {
@@ -38,7 +29,7 @@ public class JeuMemory {
     
     /**
      * 
-     * @param args : va contenir les valeurs nÈcÈssaire pour la crÈation et le dÈroulement du jeu
+     * @param args : va contenir les valeurs n√©c√©ssaire pour la cr√©ation et le d√©roulement du jeu
      */
     public static void main(String[] args)
     {
@@ -57,12 +48,12 @@ public class JeuMemory {
             delaiAffichageMauvaisePaire = Integer.parseInt(args[3]);
             numeroDeTheme = Integer.parseInt(args[4]);
         }catch (IndexOutOfBoundsException e){
-            System.out.println("Nombre de paramËtres insuffisants");
+            System.out.println("Nombre de param√®tres insuffisants");
             System.out.println("Utilisation: java Laby <nRangees> <nColonnes> <delaiAffichageInitial> <delaiAffichageMauvaisePaire> <numeroDeTheme>");
             System.out.println("Ex: java JeuMemory 4 2 3 2 2 1");
             System.exit(1);
         }catch (IllegalArgumentException e){
-            System.out.println("ParamËtres inadÈquats");
+            System.out.println("Param√®tres inad√©quats");
             System.out.println("Utilisation: java Laby <nRangees> <nColonnes> <delaiAffichageInitial> <delaiAffichageMauvaisePaire> <numeroDeTheme>");
             System.out.println("Ex: java JeuMemory 4 2 3 2 2 1");
             System.exit(1);
@@ -73,7 +64,7 @@ public class JeuMemory {
         	System.out.println("Le nombre de cases doit donner un nombre pair! Ex: 2 x 4");
         	System.exit(0);
         }
-        // Appel de la mÈthode partie pour crÈer la partie et la gÈrer
+        // Appel de la m√©thode partie pour cr√©er la partie et la g√©rer
         jeu.partie(nRangees, nColonnes, delaiAffichageInitial, delaiAffichageMauvaisePaire, numeroDeTheme);
 
     }
