1584aae0e8270faa22c1d0bd11e45a4f93f6cc34
Alexandre Lara <alexandrejml@live.com>
2015-03-28 04:48:07 -0400

    New class added: GenerateurDeCartes.java

---
+++ devoir02/src/GenerateurDeCartes.java
@@ -0,0 +1,60 @@
+
+public class GenerateurDeCartes {
+	/**
+	 * Une propriété nom comportant le nom du thème
+	 */
+	private String nom;
+	
+	/**
+	 * Une méthode retournant le nom du thème
+	 * @return le nom du thème
+	 */
+	public String getNom(){
+		return nom;
+	}
+	
+	/**
+	 * Une méthode abstraite qui retournera une référence 
+	 * de type Carte à une carte choisie aléatoirement parmi celles du thème.
+	 * 
+	 * @return une référence de type Carte
+	 */
+	public abstract Carte genereUneCarte() {
+		
+	}
+	
+	/**
+	 * Une méthode abstraite qui retournera un entier indiquant le nombre de 
+	 * cartes différentes disponibles dans ce thème
+	 * 
+	 * @return un entier indiquant le nombre de cartes différentes disponibles 
+	 * dans ce thème
+	 */
+	public abstract int nombreDeCartesDifferentes(){
+		
+	}
+	
+	/**
+	 * Une méthode Carte[] genereCartes(int n) qui générera un tableau de n 
+	 * cartes selon le thème, si possible toutes différentes, en faisant des 
+	 * appels répétés à genereUneCarte.
+	 * 
+	 * @param n number of cards in an array
+	 * @return an array of cards
+	 */
+	public Carte[] genereCartes(int n){
+		
+	}
+	
+	/**
+	 * Une méthode Carte[] generePairesDeCartesMelangees(int n) qui générera 2n 
+	 * cartes par paires identiques, selon le thème, et les mélangera (cette méthode 
+	 * devrait faire appel à genereCartes , et aux méthodes duplique et Carte.melangeCartes).
+	 * 
+	 * @param n number of cards in an array
+	 * @return an array of cards
+	 */
+	public Carte[] generePairesDeCartesMelangees(int n){
+		
+	}
+}
